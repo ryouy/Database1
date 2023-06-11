4c095458fb54814855f9ce2eec416f9a2738bb9b
ethanethz <mosbotfar@gmail.com>
2023-06-11 20:03:14 +0900

    Dice game: initial revision

---
+++ Dice.py
@@ -0,0 +1,9 @@
+import random
+
+print('Rolling dice...')
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+sum = dice1 + dice2
+print('Die 1:', dice1)
+print('Die 2:', dice2)
+print('Total value:', sum)
